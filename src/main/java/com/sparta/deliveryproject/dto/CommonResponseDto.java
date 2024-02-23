69a85908d3f10615f692216b3fd8b2922afcc488
kudongku <kudongku@naver.com>
2024-02-23 20:43:27 +0900

    feat: CommonResponseDto 생성

---
+++ src/main/java/com/sparta/deliveryproject/dto/CommonResponseDto.java
@@ -0,0 +1,15 @@
+package com.sparta.deliveryproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponseDto {
+    private int statusCode;
+    private String message;
+}
