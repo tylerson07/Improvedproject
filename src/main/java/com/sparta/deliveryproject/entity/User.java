f8d8ebc66b844113331300480a7ca5ba4e61f22c
tyler <smooky0702@gmail.com>
2024-02-23 16:01:12 +0900

    UserFieldDeclare

--- src/main/java/com/sparta/deliveryproject/entity/User.java
+++ src/main/java/com/sparta/deliveryproject/entity/User.java
@@ -1,4 +1,39 @@
 package com.sparta.deliveryproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name ="users")
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long userID;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String address;
+
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username,String password,String address){
+        this.username = username;
+        this.password = password;
+        this.address = address;
+
+    }
+
 }
