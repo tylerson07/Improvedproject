7d5211b0fca6c6638f91c3716ad4a66cae1b937c
tyler <smooky0702@gmail.com>
2024-02-27 16:10:14 +0900

    JWT 회원가입,로그인 완료

--- src/main/java/com/sparta/deliveryproject/entity/User.java
+++ src/main/java/com/sparta/deliveryproject/entity/User.java
@@ -1,17 +1,24 @@
 package com.sparta.deliveryproject.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name ="users")
-public class User {
+public class User implements UserDetails {
 
 
     @Id
@@ -29,11 +36,35 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
-    public User(String username,String password,String address){
+    public User(String username,String password,String address,UserRoleEnum role){
         this.username = username;
         this.password = password;
         this.address = address;
-
+        this.role = role;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
