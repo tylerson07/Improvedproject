2850dd5ab62c741327c637c8618b4aa17771fd38
tyler <smooky0702@gmail.com>
2024-02-28 19:43:02 +0900

    비밀번호 기능 추가

--- src/main/java/com/sparta/deliveryproject/entity/User.java
+++ src/main/java/com/sparta/deliveryproject/entity/User.java
@@ -92,4 +92,9 @@ public class User implements UserDetails {
             this.grade = UserRankEnum.COMMON;
         }
     }
+
+    public void changePassword(String password){
+        this.password = password;
+
+    }
 }
